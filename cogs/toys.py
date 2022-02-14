ec4b05c42d390e2b7fc27b601464835da98d1c50
Liem Eldert <liemeldert@protonmail.com>
2022-02-14 13:10:52 -0500

    added image to ascii

---
+++ cogs/toys.py
@@ -0,0 +1,51 @@
+# Copyright 2021 Planet Express Labs
+# All rights reserved.
+# The only reason for taking full copyright is because of a few bad actors.
+# As long as you are using my code in good faith, we will probably not have an issue with it.
+
+import art
+import disnake
+from disnake.ext import commands
+import PIL
+from utils import images
+import requests_async as requests
+
+
+class Toys(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.slash_command(name="big-text", description="Makes the text a big boy!.")
+    async def cmd_big_text(self, inter, text: str = commands.Param(),
+                           font: str = commands.Param(desc='Python Art text2art supported fonts.', default='ascii')):
+        """
+        big boy textifer
+        :Context inter:
+        :string text:
+        """
+        try:
+            await inter.response.send_message(f"```{art.text2art(text, font=font)}```")
+        except art.artError:
+            await inter.response.send_message(f":exclamation: Something went wrong! Are you sure {font} exists within text2art?")
+
+    @commands.slash_command(name="ascii-art", description="Converts content to ASCII art.")
+    async def cmd_text_to_art(self, inter,
+                              image: disnake.Attachment = commands.Param()) -> None:
+        """
+        Converts text to ASCII art.
+        :param image: image to convert to ASCII art
+        :param text: text to convert to ASCII art
+        :param inter:
+        :return:
+        """
+        imagebytes = await requests.get(image.url)
+        ascii = images.image_to_ascii(imagebytes)
+        if ascii.length > 4000:
+            return await inter.response.send_message(f"```{ascii}```")
+        else:
+            return await inter.response.send_message("The size of the image is too large for me to send as a "
+                                                     "message!", file=disnake.File(ascii, "result.txt"))
+
+
+def setup(bot):
+    bot.add_cog(Toys(bot))
--- utils/images.py
+++ utils/images.py
@@ -1,8 +1,84 @@
+import sys, random, argparse
 import imagehash
+import numpy as np
+import math
+from PIL import Image
+
+# http://paulbourke.net/dataformats/asciiart/
+grayscale_large = r"$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
+grayscale_small = r'@%#*+=-:. '
+
 
 def compare_images(image1, image2, cutoff=5):
+    """
+    Compares how different two different images are
+    :param image1: image to compare
+    :param image2: image to compare
+    :param cutoff: accepted difference between images, 5 is a reasonable default.
+    :return:
+    """
     image1 = imagehash.average_hash(image1)
     image2 = imagehash.average_hash(image2)
     if image1 - image2 < cutoff:
         return True
     return False
+
+
+def average_luminance(image: Image) -> int:
+    """
+    Gets the average luminance of an image
+    :param image: input PIL image
+    :return: average luminance
+    """
+    im = np.array(image)
+    w, h = im.shape
+    return np.average(im.reshape(w * h))
+
+
+def image_to_ascii(image: Image, columns: int = 80, scale: float = 0.43):
+    """
+    Converts an image to ascii art
+    :param image: PIL input image
+    :param columns: Columns of the output/resolution
+    :param scale: Scale of the image, sensible default
+    :return:
+    """
+    global grayscale_large, grayscale_small
+
+    # convert to grayscale
+    image = image.convert('L')
+
+    # store dimensions
+    image_width, image_height = image.size[0], image.size[1]
+
+    width = image_width / columns
+    height = width / scale
+    rows = int(image_height / height)
+
+    # check if it's actually possible to convert
+    if columns > image_width or rows > image_height:
+        return None
+
+    ascii_image = ""
+    for row in range(rows):
+        tile1 = int(row * height)
+        tile2 = int((row + 1) * height)
+        if row == rows - 1:
+            tile2 = image_height
+        row_pixels = ""
+        for i in range(columns):
+            tile3 = int(i * width)
+            tile4 = int((i + 1) * width)
+            if i == columns - 1:
+                tile4 = image_width
+
+            final_image = image.crop((tile3, tile1, tile4, tile2))
+
+            # find average luminance
+            avg = int(average_luminance(final_image))
+            grayscale_pixel = grayscale_small[int((avg * 9) / 255)]
+            row_pixels += grayscale_pixel
+            # add char to row
+        # append row to ascii image
+        ascii_image += row_pixels + "\n"
+    return ascii_image
