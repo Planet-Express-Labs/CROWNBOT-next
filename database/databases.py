1f6d52351d91a3b178918203f3ee64052186cf16
Liem Eldert <liemeldert@protonmail.com>
2022-02-14 15:48:15 -0500

    replaced Beanie driver code and models + added new

--- database/databases.py
+++ database/databases.py
@@ -3,8 +3,9 @@ from tortoise import fields
 
 
 class Server(Model):
-    server_id = fields.BigIntField(pk=True)
-    preferred_name = fields.StringField(max_length=255, default="")
+    server_internal_id = fields.IntField(pk=True)
+    server_id = fields.BigIntField()
+    preferred_name = fields.TextField(max_length=255, default="")
     roles = fields.TextField(default="", null=True)
     premium = fields.BooleanField(default=False, null=True)
 
@@ -17,21 +18,92 @@ class Server(Model):
     image_filtering_nsfw = fields.BooleanField(default=True, null=True)
 
     filter_long_text = fields.BooleanField(default=False, null=True)
-    filter_long_text_limit = fields.IntegerField(default=100, null=True)
+    filter_long_text_limit = fields.IntField(default=100, null=True)
+
+    def __str__(self):
+        return self.preferred_name
+
+    class Meta:
+        table = "servers"
+
+
+class Logging(Model):
+    """
+    Logging settings for a server, mostly unused for the current moment.
+    """
+    server_internal_id = fields.IntField(pk=True)
+    server_id = fields.BigIntField()
+    logging_enabled = fields.BooleanField(default=False, null=True)
+    logging_channel = fields.BigIntField()
+
+    # Message state
+    message_state_channel = fields.BigIntField()
+    deleted_message = fields.BooleanField(default=False)
+    edited_message = fields.BooleanField(default=False)
+
+    # User state
+    user_state_channel = fields.BigIntField()
+    user_joined = fields.BooleanField(default=False)
+    user_left = fields.BooleanField(default=False)
+    user_banned = fields.BooleanField(default=False)
+    user_unbanned = fields.BooleanField(default=False)
+    user_muted = fields.BooleanField(default=False)
+    user_unmuted = fields.BooleanField(default=False)
+    user_nickname_changed = fields.BooleanField(default=False)
+    user_roles_changed = fields.BooleanField(default=False)
+    user_avatar_changed = fields.BooleanField(default=False)
+
+    # Announcement logging
+    announcement_channel = fields.BigIntField()
+    announcements_enabled = fields.BooleanField(default=False)
+    user_joined_announcement = fields.BooleanField(default=False)
+    user_joined_announcement_message = fields.JSONField(default="")
+
+    class Meta:
+        table = "logging"
+
+
+class Reminder(Model):
+    reminder_id = fields.IntField(pk=True)
+    reminder_name = fields.TextField(max_length=255, default="")
+    server_id = fields.BigIntField()
+    channel_id = fields.BigIntField()
+    user_id = fields.BigIntField()
+    dm = fields.BooleanField()
+    ping = fields.BooleanField(default=False)
+    created = fields.DatetimeField(auto_now_add=True)
+    metadata = fields.JSONField()
+
+    def __str__(self):
+        return self.reminder_name
+
+    class Meta:
+        table = "reminders"
 
 
 class User(Model):
-    user_id = fields.BigIntField(pk=True)
-    preferred_name = fields.StringField(max_length=255, default="")
+    # Stores information that is used to personalize settings on a specific user
+    # Also provides a way for the bot to generate a profile for a user with features that discord doesn't provide
+    user_internal_id = fields.IntField(pk=True)
+    user_id = fields.BigIntField()
+    preferred_name = fields.TextField(max_length=255, null=True)
     premium = fields.BooleanField(default=False)
-    pronouns = fields.TextField(default="", null=True)
-    gender = fields.TextField(default="", null=True)
-    birthday = fields.DatetimeField(default="", null=True)
+    pronouns = fields.TextField(null=True)
+    gender = fields.TextField(null=True)
+    birthday = fields.DatetimeField(null=True)
     private = fields.BooleanField(default=False)
+
+    def __str__(self):
+        return self.preferred_name
+
+    class Meta:
+        table = "users"
 
 
 class ConfessChannel(Model):
-    confess_id = fields.IntField(pk=True)
+    # Stores information specific to confess channels
+    intenral_id = fields.IntField(pk=True)
+    confess_id = fields.BigIntField()
     enable = fields.BooleanField(default=False, null=True)
     guild = fields.BigIntField(default=0, null=True)
     confess_channel = fields.BigIntField(default=0)
@@ -41,4 +113,8 @@ class ConfessChannel(Model):
     whitelist = fields.BooleanField(default=False, null=True)
     allowed_roles = fields.TextField(default="", null=True)
 
+    def __str__(self):
+        return str(self.confess_id)
 
+    class Meta:
+        table = "confess_channels"
--- main.py
+++ main.py
@@ -2,66 +2,113 @@
 # # All rights reserved.
 # The only reason for taking full copyright is because of a few bad actors.
 # As long as you are using my code in good faith, we will probably not have an issue with it.
+import os
+
 import disnake
 from disnake.ext import commands
 from tortoise import Tortoise
 from utils.admin import admin_command
 from zoidberg.config import *
+import signal
+import sys
 
 __version__ = "3.0 PRE"
 
-# define activity, playing status
 activity = disnake.Activity(name='> planetexpresslabs.io', type=disnake.ActivityType.playing)
+
+# define activity, playing status
 # define gateway intents
 intents = disnake.Intents.default()
 intents.members = True
+# TODO: migrate to config
 bot = commands.Bot(command_prefix=commands.when_mentioned,
                    activity=activity,
                    intents=intents,
-                   # TODO place this the config file once again.
-                   test_guilds=[842987183588507670])
+                   test_guilds=[842987183588507670, 769039315945914370])
 
 logging.basicConfig(level=logging.INFO)
-for filename in os.listdir("cogs"):
+
+print(DISABLED_COGS)
+for filename in os.listdir(Path('cogs/')):
     if filename.endswith(".py") and filename not in DISABLED_COGS:
         bot.load_extension(f"cogs.{filename[:-3]}")
 
+
 async def init():
-    # Here we connect to a SQLite DB file.
-    # also specify the app name of "models"
-    # which contain models from "app.models"
+
+    # Don't generate schema on first run to prevent wiping DB.
+    if os.getenv('PEXL_first_run') is None:
+        print("First run detected. Rerun the bot to initialize the DB.")
+        os.environ['PEXL_first_run'] = '1'
+        print(os.getenv('PEXL_first_run'))
+        sys.exit(1)
+
+    if os.getenv("PEXL_first_run") == '1':
+        # Generate the schema on first run only
+        print("Second/First run detected. Generating schema...")
+        os.environ["PEXL_first_run"] = '2'
+        await Tortoise.init(
+            db_url=CONNURL,
+            modules={'models': ['database.databases']},
+            _create_db=True
+        )
+        await Tortoise.generate_schemas()
+        return
+    if os.getenv("PEXL_force_regen_schema") == '69':
+        print("Wiping schema on DB...\nYou asked for it. ")
+        await Tortoise.init(
+            db_url=CONNURL,
+            modules={'models': ['database.databases']},
+            _create_db=True
+        )
+        await Tortoise.generate_schemas(safe=False)
+        return
+
     await Tortoise.init(
-        db_url=CONNURL,
-        modules={'models': ['app.models']}
-    )
-    # Generate the schema
-    await Tortoise.generate_schemas()
-        
-    
+            db_url=CONNURL,
+            modules={'models': ['database.databases']}
+        )
+
+
+async def handler() -> None:
+    """
+    Handler for SIGTERM.
+    Prevents DB loss by gracefully closing connections.
+    """
+    print("SIGTERM received. Closing event loops.")
+    await bot.close()
+    print("Closing DB connections...")
+    await Tortoise.close_connections()
+    print("DB connections closed. Exiting program. See you soon! Goodbye!")
+    sys.exit(1)
+
+
 @bot.event
 async def on_ready():
     # Initialize our databases
     await init()
-    print(f"Bot is ready, logged in as {bot.user.name} ({bot.user.id}).")
     await bot.wait_until_ready()
+    print(f"Bot is ready, logged in as {bot.user.name} ({bot.user.id}).")
 
 
-@bot.slash_command(name='foo', brief='Tests if the bot is dead or not')
+@bot.slash_command(name='foo', description='Tests if the bot is dead or not')
 async def cmd_foo(ctx):
     await ctx.response.send_message(f"Bar!\nLatency: {bot.latency} ms")
 
 
-@bot.command(name="resetcogs")
+@bot.command(name="resetcogs", brief="reloads all the cogs")
 @admin_command
 async def cmdadmin_resetcogs(ctx):
     """
     Reset all cogs.
     """
-    for cog in bot.cogs:
-        if cog not in DISABLED_COGS:
+    for cog in os.listdir(Path('cogs/')):
+        if filename.endswith(".py") and cog not in DISABLED_COGS:
             bot.unload_extension(cog)
             bot.load_extension(cog)
-    return await ctx.reply("Cogs reset.")
+    await ctx.message.delete()
+    return await ctx.reply("Cogs reset.", delete_after=5)
 
-
+# Gracefully handle Heroku's SIGTERM signal to prevent DB data loss.
+signal.signal(signal.SIGTERM, handler)
 bot.run(BOT_TOKEN)
