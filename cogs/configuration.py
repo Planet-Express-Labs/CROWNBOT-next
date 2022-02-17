0321ad3594bd99e3e5c63f6ed28faff399ec5ac7
Liem Eldert <liemeldert@protonmail.com>
2022-02-17 08:37:20 -0500

    moved this to a more logical location and made it more finished

--- cogs/configuration.py
+++ cogs/configuration.py
@@ -4,97 +4,9 @@
 # As long as you are using my code in good faith, we will probably not
 # have an issue with it.
 
-from typing import List
-from tortoise import Tortoise
+
 import disnake
 from disnake.ext import commands
-import re
-
-
-class Menu(disnake.ui.View):
-    def __init__(self, embeds: List[disnake.Embed]):
-        super().__init__(timeout=None)
-        self.embeds = embeds
-        self.embed_count = 0
-
-        self.first_page.disabled = True
-        self.prev_page.disabled = True
-        self.additional_components = []
-
-        # Sets the footer of the embeds with their respective page numbers.
-        for i, embed in enumerate(self.embeds):
-            embed.set_footer(text=f"Page {i + 1} of {len(self.embeds)}")
-            terms = re.findall(r"%([^;]*)", embed.description)
-            if any(terms):
-                embed.description = embed.description.replace(
-                    f"%{terms[0]}", "")
-                for each in terms:
-                    if each[0:7] == "%TOGGLE":
-                        params = each.replace(
-                            '(', '').replace(')', '').split(', ')
-                        models = Tortoise.describe_models()
-                        for model in models:
-                            name = model.__name__
-                            if name.lower == params[0]:
-                                break
-
-                        self.additional_components.append(
-                            disnake.ui.Button(
-                                label=params[2],
-                                custom_id=params[3]
-                            )
-                        )
-
-    @disnake.ui.button(emoji="⏪", style=disnake.ButtonStyle.blurple)
-    async def first_page(self, interaction: disnake.MessageInteraction):
-        self.embed_count = 0
-        embed = self.embeds[self.embed_count]
-        embed.set_footer(text=f"Page 1 of {len(self.embeds)}")
-
-        self.first_page.disabled = True
-        self.prev_page.disabled = True
-        self.next_page.disabled = False
-        self.last_page.disabled = False
-        await interaction.response.edit_message(embed=embed, view=self)
-
-    @disnake.ui.button(emoji="◀")
-    async def prev_page(self, interaction: disnake.MessageInteraction):
-        self.embed_count -= 1
-        embed = self.embeds[self.embed_count]
-
-        self.next_page.disabled = False
-        self.last_page.disabled = False
-        if self.embed_count == 0:
-            self.first_page.disabled = True
-            self.prev_page.disabled = True
-        await interaction.response.edit_message(embed=embed, view=self)
-
-    @disnake.ui.button(emoji="❌", style=disnake.ButtonStyle.red)
-    async def remove(self, interaction: disnake.MessageInteraction):
-        await interaction.response.edit_message(view=None)
-
-    @disnake.ui.button(emoji="▶")
-    async def next_page(self, interaction: disnake.MessageInteraction):
-        self.embed_count += 1
-        embed = self.embeds[self.embed_count]
-
-        self.first_page.disabled = False
-        self.prev_page.disabled = False
-        if self.embed_count == len(self.embeds) - 1:
-            self.next_page.disabled = True
-            self.last_page.disabled = True
-        await interaction.response.edit_message(embed=embed, view=self)
-
-    @disnake.ui.button(emoji="⏩", style=disnake.ButtonStyle.blurple)
-    async def last_page(self, interaction: disnake.MessageInteraction):
-        self.embed_count = len(self.embeds) - 1
-        embed = self.embeds[self.embed_count]
-
-        self.first_page.disabled = False
-        self.prev_page.disabled = False
-        self.next_page.disabled = True
-        self.last_page.disabled = True
-        await interaction.response.edit_message(embed=embed, view=self)
 
 
 class Configuration(commands.Cog):
@@ -136,8 +48,8 @@ Hash filtering uses our database to detect common images that may not be detecte
 These images usually don't contain nudity, but are still extremely suggestive
 
 You can configure which channels will use this filter on the next page.
-%TOGGLE(Server, image_filtering, enable_image_filtering)
-%TOGGLE(Server, hash_filtering, enable_image_filtering)
+%TOGGLE(Server, image_filtering, enable image filtering);
+%TOGGLE(Server, hash_filtering, enable image filtering);
         """,
                 colour=disnake.Color.random(),
             ),
--- utils/paginate.py
+++ utils/paginate.py
@@ -1,3 +1,128 @@
+# Copyright 2021 Planet Express Labs
+# All rights reserved.
+# The only reason for taking full copyright is because of a few bad actors.
+# As long as you are using my code in good faith, we will probably not
+# have an issue with it.
+import re
+from typing import List
+
+import disnake
+from tortoise import Tortoise
+
+
+class Menu(disnake.ui.View):
+    def __init__(self, embeds: List[disnake.Embed]):
+        """
+        Discord embed based menu system.
+        :param embeds: A list of embeds to be displayed in the menu.
+        """
+        super().__init__(timeout=None)
+        self.embeds = embeds
+        self.embed_count = 0
+
+        self.first_page.disabled = True
+        self.prev_page.disabled = True
+        self.additional_components = []
+        self.model_components = []
+
+        # Sets the footer of the embeds with their respective page numbers.
+        for i, embed in enumerate(self.embeds):
+            embed.set_footer(text=f"Page {i + 1} of {len(self.embeds)}")
+            terms = re.findall(r"%([^;]*)", embed.description)
+            # grab the button declarations
+            if any(terms):
+                embed.description = embed.description.replace(
+                    f"%{terms[0]}", "")
+                for each in terms:
+                    if each[0:7] == "%TOGGLE":
+                        params = each.replace(
+                            '(', '').replace(')', '').split(', ')
+                        models = Tortoise.describe_models()
+                        for model in models:
+                            name = model.__name__
+                            if name.lower == params[0]:
+                                self.model_components.append((params[3], model))
+                                break
+                        self.additional_components.append(
+                            disnake.ui.Button(
+                                label=params[2],
+                                custom_id=params[3]
+                            )
+                        )
+
+    @disnake.ui.button(emoji="⏪", style=disnake.ButtonStyle.blurple)
+    async def first_page(self, interaction: disnake.MessageInteraction):
+        self.embed_count = 0
+        embed = self.embeds[self.embed_count]
+        embed.set_footer(text=f"Page 1 of {len(self.embeds)}")
+
+        self.first_page.disabled = True
+        self.prev_page.disabled = True
+        self.next_page.disabled = False
+        self.last_page.disabled = False
+        await interaction.response.edit_message(embed=embed, view=self)
+
+    @disnake.ui.button(emoji="◀")
+    async def prev_page(self, interaction: disnake.MessageInteraction):
+        self.embed_count -= 1
+        embed = self.embeds[self.embed_count]
+
+        self.next_page.disabled = False
+        self.last_page.disabled = False
+        if self.embed_count == 0:
+            self.first_page.disabled = True
+            self.prev_page.disabled = True
+        await interaction.response.edit_message(embed=embed, view=self)
+
+    @disnake.ui.button(emoji="❌", style=disnake.ButtonStyle.red)
+    async def remove(self, interaction: disnake.MessageInteraction):
+        await interaction.response.edit_message(view=None)
+
+    @disnake.ui.button(emoji="▶")
+    async def next_page(self, interaction: disnake.MessageInteraction):
+        self.embed_count += 1
+        embed = self.embeds[self.embed_count]
+
+        self.first_page.disabled = False
+        self.prev_page.disabled = False
+        if self.embed_count == len(self.embeds) - 1:
+            self.next_page.disabled = True
+            self.last_page.disabled = True
+        await interaction.response.edit_message(embed=embed, view=self)
+
+    @disnake.ui.button(emoji="⏩", style=disnake.ButtonStyle.blurple)
+    async def last_page(self, interaction: disnake.MessageInteraction):
+        self.embed_count = len(self.embeds) - 1
+        embed = self.embeds[self.embed_count]
+
+        self.first_page.disabled = False
+        self.prev_page.disabled = False
+        self.next_page.disabled = True
+        self.last_page.disabled = True
+        await interaction.response.edit_message(embed=embed, view=self)
+
+    async def interaction_check(self, inter: disnake.MessageInteraction):
+        for each in self.model_components:
+            if each[0] == inter.component.custom_id:
+
+
+
+
+
+# class MenuEmbed(disnake.Embed):
+#     def __init__(self, *,
+#                  colour: Union[int, Colour, _EmptyEmbed] = EmptyEmbed,
+#                  color: Union[int, Colour, _EmptyEmbed] = EmptyEmbed,
+#                  title: MaybeEmpty[Any] = EmptyEmbed,
+#                  type: EmbedType = "rich",
+#                  url: MaybeEmpty[Any] = EmptyEmbed,
+#                  description: MaybeEmpty[Any] = EmptyEmbed,
+#                  timestamp: datetime.datetime = None,
+#                  ):
+#         super().__init__(colour=colour, color=color, title=title, type=type, url=url, description=description,
+#                          timestamp=timestamp)
+
+
 async def progress_bar(iteration, total, prefix='', suffix='', decimals=1, length=100, fill='█'):
     """
     Call in a loop to create terminal progress bar
